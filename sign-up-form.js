6120c7eb6dd1cfb9af555b5f28706c234b7394c0
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-11-09 18:32:21 +0200

    Fix validation

--- sign-up-form.js
+++ sign-up-form.js
@@ -1,13 +1,16 @@
 function validateForm() {
     const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     const email = document.forms["sign-up-form"]["email"].value;
-    if (email.match(mailformat)) {
-        alert("Valid email address!");
-        return true;
-    }
-    else {
+    const password = document.forms["sign-up-form"]["password"].value;
+    const confirmPassword = document.forms["sign-up-form"]["confirm-password"].value;
+    if (!email.match(mailformat)) {
         alert("You have entered an invalid email address!");
         document.forms["sign-up-form"]["email"].focus();
         return false;
     }
+    else if (password !== confirmPassword) {
+        alert("Passowrd and Confirm Password do not match");
+        return false
+    }
+    return true;
 }
